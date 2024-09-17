41d2ae65ee43d35400bd1fd95fd9b299c4f7654d
malik <malik.bazil@student.umn.ac.id>
2024-09-17 15:39:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab_week_04/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lab_week_04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lab_week_04.R
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
